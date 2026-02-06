333c221838ab6fd973ce5955223a6ed192061695
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2026-02-06 13:17:21 +0530

    demo1 job

---
+++ file1.py
@@ -0,0 +1,18 @@
+def welcome():
+    print("welcome to python sessions")
+
+welcome()
+
+def currentyear():
+    return "2026"
+
+year=currentyear()
+print(year)
+
+def add(x,y):
+    result=x+y
+    return result
+
+
+sum=add(20,30)
+print(sum)
